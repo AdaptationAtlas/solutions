a82f9130baf185ef62b3d9362992e3a240c06780
peetmate <peetmate@gmail.com>
2022-07-20 12:21:41 +0300

    Missed scripts added

---
+++ analogues/aquire_datasets.R
@@ -0,0 +1,4 @@
+devtools::source_url("https://github.com/EiA2030/source_data/blob/main/R/soilgrids250_download.R?raw=TRUE")
+
+
+soilgrids250_download(par = "soc", depth = "5-15", path = tempdir(), lonlat=NULL)
