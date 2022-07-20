305fdbd35ff7d18be614e5592e07bf9d62dde206
Pete Steward <p.steward@cgiar.org>
2022-07-20 11:44:35 +0000

    Data aquisition for soilgrids added

--- analogues/aquire_datasets.R
+++ analogues/aquire_datasets.R
@@ -1,4 +1,14 @@
 devtools::source_url("https://github.com/EiA2030/source_data/blob/main/R/soilgrids250_download.R?raw=TRUE")
-
-
 soilgrids250_download(par = "soc", depth = "5-15", path = tempdir(), lonlat=NULL)
+
+c("BLDFIE","CECSOL","CLYPPT","SNDPPT","SLTPPT","ORCDRC","PHIHOX","AWCh3")
+
+Path<-"/home/jovyan/common_data/soilgrids/raw/"
+Vars<-c("bdod","cec","clay","sand","silt","soc","phh20")
+Depths<-c("0-5","5-15","15-30","30-60","60-100")
+
+lapply(Vars,FUN=function(VAR){
+terra::rast(paste0(Path,VAR,"_",Depths,".vrt"))
+})
+
+
