f905b730d5c2e188d022392fbf370bf996033630
Pete Steward <p.steward@cgiar.org>
2022-07-21 13:33:51 +0000

    worldclim updates finished

--- analogues/aquire_datasets.R
+++ analogues/aquire_datasets.R
@@ -17,7 +17,7 @@ if(!dir.exists(BoundIntDir)){
     }
 
 # Dissolve borders
-if(!file.exists(paste0(BoundIntDir,"gadml0_4326_agg.shp")){
+if(!file.exists(paste0(BoundIntDir,"gadml0_4326_agg.shp"))){
     sh_ctry<-terra::vect("/home/jovyan/common_data/atlas/raw/0_boundaries/gadml0_4326.shp")
     sh_ctry <- terra::project(sh_ctry, "+proj=longlat +ellps=WGS84 +no_defs")
     sh_africa<-terra::aggregate(sh_ctry)
@@ -59,19 +59,60 @@ WCDir<-paste0(RawDir,"worldclim/")
 if(!dir.exists(WCDir)){
     dir.create(WCDir)
 }
-   
-for (tvar in c("prec","tmin","tmean","tmax")) {
-  #tvar <- "prec"
-  cat("downloading",tvar,"\n")
-  if (!file.exists(paste(WCDir,"wc_",tvar,".rda",sep=""))) {
-    wc_data <- raster::getData('worldclim', var=tvar, res=2.5, path=WCDir)
-    wc_data <- terra::rast(wc_data)
-    wc_data <- terra::mask(terra::crop(wc_data, sh_africa),sh_africa)
-    wc_data <- raster::raster(wc_data)
-    wc_data <- readAll(wc_data)
-    assign(paste("wc_",tvar,sep=""), wc_data)
-    rm(wc_data)
-    save(list=c(paste("wc_",tvar,sep="")), file=paste(WCDir,"wc_",tvar,".rda",sep=""), compress="xz",compression_level=9)
-    unlink(paste(WCDir,"wc2-5",sep=""),recursive=T,force=T)
-  }
+
+WCDirInt<-paste0(IntDir,"worldclim/")
+if(!dir.exists(WCDirInt)){
+    dir.create(WCDirInt)
 }
+      
+# Resolution = c("10m","5m","2.5m","30s")
+# Variable = c("tmin","tmax","tavg","prec","srad","wind","vapr")
+GetWorldClim<-function(Variable,Resolution,SaveDir){
+     if(!dir.exists(SaveDir)){
+      dir.create(SaveDir,recursive=T)
+    }
+    
+    for(VAR in Variable){
+        URL<-paste0("https://biogeo.ucdavis.edu/data/worldclim/v2.1/base/wc2.1_",Resolution,"_",VAR,".zip")
+        destfile<-paste0(SaveDir,"wc2.1_",Resolution,"_",VAR,".zip")
+        # Display progress
+        cat('\r                                                ')
+        cat('\r',paste0("Downloading file: ",VAR))
+        flush.console()
+        
+        if(!file.exists(destfile)){
+            download.file(URL, destfile)
+        } 
+    }
+}
+
+Resolution<-"2.5m"
+Variables<-c("tmin","tmax","tavg","prec")
+
+GetWorldClim(Variable=Variables,Resolution=Resolution,SaveDir=WCDir)
+      
+wc_data<-lapply(Variables,FUN=function(VAR){
+  
+  if (!file.exists(paste0(WCDirInt,"wc2.1_",Resolution,"_",VAR,"01_masked.tif",sep=""))) {
+    
+      unzip(paste0(WCDir,"wc2.1_",Resolution,"_",VAR,".zip"),exdir=WCDir)
+    
+      Files <- paste0(WCDir,unzip(paste0(WCDir,"wc2.1_",Resolution,"_",VAR,".zip"),list=T,exdir=WCDir)$Name)
+      wc_data <- terra::rast(Files[!grepl("readme",Files)])     
+      wc_data <- terra::mask(terra::crop(wc_data, sh_africa),sh_africa)
+                
+      lapply(names(wc_data),FUN=function(LAYER){
+        terra::writeRaster(wc_data[LAYER],paste0(WCDirInt,LAYER,"_masked.tif"))
+      })            
+      unlink(Files)
+      wc_data
+      
+  }else{
+      
+      Files<-list.files(WCDirInt,VAR,full.names=T) 
+      Files<-Files[!grepl("zip",Files)]
+      terra::rast(Files)
+      }
+})
+
+names(wc_data)<-Variables
