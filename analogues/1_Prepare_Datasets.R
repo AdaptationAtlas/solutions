9b0bc5615220bb758f8be2baa447be921308d1ff
Pete Steward <p.steward@cgiar.org>
2022-07-21 17:44:06 +0000

    CMIP6 worldclim added

--- analogues/1_Prepare_Datasets.R
+++ analogues/1_Prepare_Datasets.R
@@ -26,17 +26,13 @@ if(!file.exists(paste0(BoundIntDir,"gadml0_4326_agg.shp"))){
     sh_africa<-terra::vect(paste0(BoundIntDir,"gadml0_4326_agg.shp"))
 }
 
-# Get worldclim climate data ####
+# Get historic worldclim climate data ####
 WCDir<-paste0(RawDir,"worldclim/")
 if(!dir.exists(WCDir)){
     dir.create(WCDir)
 }
 
-WCDirInt<-paste0(IntDir,"worldclim/")
-if(!dir.exists(WCDirInt)){
-    dir.create(WCDirInt)
-}
-      
+     
 # Resolution = c("10m","5m","2.5m","30s")
 # Variable = c("tmin","tmax","tavg","prec","srad","wind","vapr")
 GetWorldClim<-function(Variable,Resolution,SaveDir){
@@ -62,7 +58,13 @@ Resolution<-"2.5m"
 Variables<-c("tmin","tmax","tavg","prec")
 
 GetWorldClim(Variable=Variables,Resolution=Resolution,SaveDir=WCDir)
-      
+
+# Process worldclim data
+WCDirInt<-paste0(IntDir,"worldclim/")
+if(!dir.exists(WCDirInt)){
+    dir.create(WCDirInt)
+}
+
 wc_data<-lapply(Variables,FUN=function(VAR){
   
   if (!file.exists(paste0(WCDirInt,"wc2.1_",Resolution,"_",VAR,"01_masked.tif",sep=""))) {
@@ -88,6 +90,41 @@ wc_data<-lapply(Variables,FUN=function(VAR){
 })
 
 names(wc_data)<-Variables
+
+# Get worldclim CMIP6 ####
+WC_CMIPDir<-paste0(RawDir,"worldclim/CIMP6/")
+if(!dir.exists(WCDir)){
+    dir.create(WCDir)
+}
+
+GetWorldClimGCMs<-function(Variable,GCMs,Scenarios,SaveDir){
+     if(!dir.exists(SaveDir)){
+      dir.create(SaveDir,recursive=T)
+    }
+          
+    for(VAR in Variable){
+        for(SCENARIO in Scenarios){
+            for(GCM in GCMs){      
+                URL<-paste0("https://geodata.ucdavis.edu/cmip6/2.5m/",GCM,"/",Scenarios,"/wc2.1_2.5m_",VAR,"_",GCM,"_",SCENARIO,"_2041-2060.zip")
+                destfile<-paste0(SaveDir,"wc2.1_",Resolution,"_",VAR,".zip")
+                # Display progress
+                cat('\r                                                           ')
+                cat('\r',paste0("Downloading file: ",VAR,"-",SCENARIO,"-",GCM))
+                flush.console()
+                
+                if(!file.exists(destfile)){
+                    download.file(URL, destfile)
+                } 
+            }
+        }
+    }
+}
+
+GCMs<-c("ACCESS-CM2","ACCESS-ESM1-5","BCC-CSM2-MR","CanESM5","CanESM5-CanOE","CMCC-ESM2","CNRM-CM6-1","CNRM-CM6-1-HR","CNRM-ESM2-1","EC-Earth3-Veg","EC-Earth3-Veg-LR","FIO-ESM-2-0","GFDL-ESM4","GISS-E2-1-G","GISS-E2-1-H","HadGEM3-GC31-LL","INM-CM4-8","INM-CM5-0","IPSL-CM6A-LR","MIROC-ES2L","MIROC6","MPI-ESM1-2-HR","MPI-ESM1-2-LR","MRI-ESM2-0","UKESM1-0-LL")
+Scenarios<-c("ssp126","ssp245","ssp370","ssp585")
+Variables<-c("tmin","tmax","prec")
+
+GetWorldClimGCMs(Variable=Variables,Scenarios=Scenarios,GCMs=GCMs,SaveDir=WCDir)
 
 # Create a mask ####
 msk <-wc_data[["prec"]][[1]]
