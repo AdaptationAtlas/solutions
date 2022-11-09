1f5bdab17b2ceb4c48c37d08f158b7701589da0d
Pete Steward <p.steward@cgiar.org>
2022-11-09 13:59:28 +0000

    Crop specific areas added

--- analogues/R/4_Classify_Thresholds_Limits.R
+++ analogues/R/4_Classify_Thresholds_Limits.R
@@ -24,10 +24,13 @@ cimdir_vr<-paste0(DataDir,"/atlas_analogues/intermediate/v",Version)
 results_dir<-paste0(cimdir_vr,"/results_mean")
 
 # Save directory for classified files
-save_dir<-paste0(cimdir_vr,"/results_mean_classified")
+save_dir<-paste0(cimdir_vr,"/results_mean_class_crop")
 if(!dir.exists(save_dir)){
   dir.create(save_dir,recursive=T)
 }
+
+# Use a mask derived from all mapspam crops present in ERA data, or crop specific data
+use_generic_mask<-F
 
 # Read in admin0 for subsaharan africa
 adm1_africa<-terra::vect(paste0(DataDir,"/atlas_boundaries/intermediate/gadm41_ssa_1.shp"))
@@ -50,12 +53,22 @@ msfiles<-list.files(paste0(DataDir,"/mapspam_2017/raw"),"_A_",full.names=T)
 msfiles<-grep("_A.tif",msfiles,value=T)
 ms_crops_area<-terra::rast(msfiles)
 ms_crops_area<-terra::mask(terra::crop(ms_crops_area,adm1_africa),adm1_africa)
-ms_crops_area<-sum(ms_crops_area)
 ms_crops_area<-100*ms_crops_area/terra::cellSize(ms_crops_area,unit="ha")
 
-# Create MapSPAM crop mask
-Threshold<-0.01
-generic_crop_mask<-terra::classify(ms_crops_area,cbind(c(0,Threshold),c(Threshold,9999),c(0,1)))
+# harmonize crop names
+era2ms<-data.table::fread("https://github.com/AdaptationAtlas/solutions/blob/main/analogues/Data/ERA2MapSPAM.csv?raw=T")
+names(ms_crops_area)<-gsub("spam2017V2r1_SSA_A_","",names(ms_crops_area))
+names(ms_crops_area)<-gsub("_A","",names(ms_crops_area))
+names(ms_crops_area)<-era2ms[match(names(ms_crops_area),MapSPAM_code),ERA]
+ms_crops_area<-ms_crops_area[[names(ms_crops_area)!="NA"]]
+
+# Create MapSPAM crop mask (%)
+Threshold<-1
+
+# All crops summed
+generic_crop_mask<-terra::classify(sum(ms_crops_area),cbind(c(0,Threshold),c(Threshold,9999),c(NA,1)))
+
+ms_crops_area<-terra::classify(ms_crops_area,cbind(c(0,Threshold),c(Threshold,9999),c(NA,1)))
 
 
 # Set values each yield threshold will take, make sure thresholds are in ascending order
@@ -68,6 +81,16 @@ X<-as.numeric(gsub(".tif","",unlist(lapply(strsplit(Files,"-"),tail,1))))
 
 Files<-Files[X>=10]
 
+# Make vector of crop names to link to map spam area by crop
+Crop<-rep("",length(Files))
+Crop[grepl("baseline",Files)]<-unlist(tstrsplit(Files[grepl("baseline",Files)],"-",keep=3))
+Crop[!grepl("baseline",Files)]<-unlist(tstrsplit(Files[!grepl("baseline",Files)],"-",keep=4))
+Crop[grepl("baseline-Mulch-Red",Files)]<-unlist(tstrsplit(Files[grepl("baseline-Mulch-Red",Files)],"-",keep=4))
+Crop[!grepl("baseline-Mulch-Red",Files) & grepl("Mulch-Red",Files)]<-unlist(tstrsplit(Files[!grepl("baseline-Mulch-Red",Files)  & grepl("Mulch-Red",Files)],"-",keep=5))
+Crop<-gsub("_"," ",Crop)
+
+# Overwrite?
+Overwrite<-T
 
 for(i in 1:length(Files)){
   
@@ -75,7 +98,7 @@ for(i in 1:length(Files)){
   cat("Processing ", i, "of", length(Files))
   flush.console()
   
-  if(!file.exists(paste0(save_dir,"/",Files[i]))){
+  if((!file.exists(paste0(save_dir,"/",Files[i])))|Overwrite){
     Rast<-terra::rast(paste0(results_dir,"/",Files[i]))
     Rast<-terra::resample(Rast,base_raster)
     
@@ -83,12 +106,16 @@ for(i in 1:length(Files)){
                                                     to=log(c(1,1.1,1.25,1.5,9999)),
                                                     val=c(0,1,2,3,4))))
     
-    Rast[is.na(Rast) & !is.na(generic_crop_mask)]<--9999
+      if(use_generic_mask){
+          Rast[is.na(Rast) & !is.na(generic_crop_mask)]<--9999
+          }else{
+           Rast[is.na(Rast) & !is.na(ms_crops_area[[Crop[i]]])]<--9999
+      }
     
     # Assign levels to the raster
     levels(Rast)<-Tvals
     
-    terra::writeRaster(Rast,paste0(save_dir,"/",Files[i]))
+    terra::writeRaster(Rast,paste0(save_dir,"/",Files[i]),overwrite=Overwrite)
   }
 }
 
